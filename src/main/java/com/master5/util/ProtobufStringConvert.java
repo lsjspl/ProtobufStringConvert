4048cf96bd2c9f8912a5f3292b81eb62f79a9a18
liusanjun <liusanjun@LAPTOP-A3EI3LBC>
2016-12-12 11:42:41 +0800

    变量大写

--- src/main/java/com/master5/util/ProtobufStringConvert.java
+++ src/main/java/com/master5/util/ProtobufStringConvert.java
@@ -8,21 +8,21 @@ import java.util.Stack;
 
 public class ProtobufStringConvert {
 	
-	private static String lineSplitRex="[\n\r]";
+	private static String LINE_SPLIT_REX="[\n\r]";
 	
-	private static String codeOpen="{";
+	private static String CODE_OPEN="{";
 	
-	private static String codeClose="}";
+	private static String CODE_CLOSE="}";
 	
-	private static String codeStringContentRex="\"[\\s\\S]*?\"";
+	private static String CODE_STRING_CONTENT_REX="\"[\\s\\S]*?\"";
 	
-	private static String codeStringFlag="\"";
+	private static String CODE_STRING_FLAG="\"";
 	
-	private static String codeTrueFlag="true";
+	private static String CODE_TRUE_FLAG="true";
 	
-	private static String codeFalseFlag="false";
+	private static String CODE_FALSE_FLAG="false";
 	
-	private static String codeDoubleFlag=".";
+	private static String CODE_DOUBLE_FLAG=".";
 	
 	
 	
@@ -34,7 +34,7 @@ public class ProtobufStringConvert {
 	@SuppressWarnings("unchecked")
 	public static Map<String,Object> coventStringToMap(String protobufString){
 	
-			String[] results = protobufString.split(lineSplitRex);
+			String[] results = protobufString.split(LINE_SPLIT_REX);
 			String[] resultsTmp;
 			String compareStr;
 			Stack<Map<String, Object>> stack = new Stack<>();
@@ -46,15 +46,15 @@ public class ProtobufStringConvert {
 			Object valueObject;
 
 			for (String result : results) {
-				compareStr = result.replaceAll(codeStringContentRex, "").trim();
-				if (compareStr.startsWith(codeOpen) || compareStr.endsWith(codeOpen)) {//判断开始符号
+				compareStr = result.replaceAll(CODE_STRING_CONTENT_REX, "").trim();
+				if (compareStr.startsWith(CODE_OPEN) || compareStr.endsWith(CODE_OPEN)) {//判断开始符号
 					cache = new HashMap<String, Object>();
-					stack.peek().put(result.replace(codeOpen, "").trim(), cache);
+					stack.peek().put(result.replace(CODE_OPEN, "").trim(), cache);
 					stack.push(cache);
 					continue;
 				}
 
-				if (compareStr.startsWith(codeClose) || compareStr.endsWith(codeClose)) {//判断结束符号
+				if (compareStr.startsWith(CODE_CLOSE) || compareStr.endsWith(CODE_CLOSE)) {//判断结束符号
 					stack.pop();
 					continue;
 				}
@@ -64,16 +64,16 @@ public class ProtobufStringConvert {
 				value = resultsTmp[1].trim();
 				cache = stack.peek();
 
-				if (value.equals(codeFalseFlag) || value.equals(codeTrueFlag)) {
+				if (value.equals(CODE_FALSE_FLAG) || value.equals(CODE_TRUE_FLAG)) {
 					valueObject = Boolean.parseBoolean(value);
-				} else if (!value.startsWith(codeStringFlag)) {//以引号开始是字符串
-					if (value.contains(codeDoubleFlag)) {//浮点型
+				} else if (!value.startsWith(CODE_STRING_FLAG)) {//以引号开始是字符串
+					if (value.contains(CODE_DOUBLE_FLAG)) {//浮点型
 						valueObject = Double.parseDouble(value);
 					} else {
 						valueObject = Integer.parseInt(value);
 					}
 				} else {
-					if (value.startsWith(codeStringFlag)) {
+					if (value.startsWith(CODE_STRING_FLAG)) {
 						valueObject = value.substring(1, value.length() - 1);
 					} else {
 						valueObject = value;
